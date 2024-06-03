603a5113c63bea9a7244976994afe913697cb523
Chillsome <mojica.david123@gmail.com>
2024-06-03 02:19:58 -0500

    Nvim changes

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -35,21 +35,21 @@ M.ui = {
     },
   },
 
-  hl_override = {
-    Comment = { italic = true },
-    ["@comment"] = { italic = true },
-    DiffChange = {
-      bg = "#464414",
-      fg = "none",
-    },
-    DiffAdd = {
-      bg = "#103507",
-      fg = "none",
-    },
-    DiffRemoved = {
-      bg = "#461414",
-      fg = "none",
-    },
-  },
+  -- hl_override = {
+  --   Comment = { italic = true },
+  --   ["@comment"] = { italic = true },
+  --   DiffChange = {
+  --     bg = "#464414",
+  --     fg = "none",
+  --   },
+  --   DiffAdd = {
+  --     bg = "#103507",
+  --     fg = "none",
+  --   },
+  --   DiffRemoved = {
+  --     bg = "#461414",
+  --     fg = "none",
+  --   },
+  -- },
 }
 return M
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,11 +1,11 @@
 return {
-  -- {
-  --   "stevearc/conform.nvim",
-  --   -- event = 'BufWritePre', -- uncomment for format on save
-  --   config = function()
-  --     require "configs.conform"
-  --   end,
-  -- },
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
 
   -- These are some examples, uncomment them if you want to see them work!
   {
@@ -61,6 +61,9 @@ return {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
+        "gopls",
+        "rust-analyzer",
+        "clangd",
         "lua-language-server",
         "stylua",
         "html-lsp",
